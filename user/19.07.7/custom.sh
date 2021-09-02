ef8184b6eb5bcd542c8ab0b5c6b925fff41bbf1f
mrG1K <mr@g1k.ru>
2021-09-02 14:25:48 +0300

    feeds.conf default

--- user/19.07.7/custom.sh
+++ user/19.07.7/custom.sh
@@ -10,7 +10,6 @@ echo "=============================================="
 rm ./package/feeds/packages/node
 rm ./package/feeds/packages/node-*
 ./scripts/feeds install -a -p node
-./scripts/feeds install -a -p node
 make defconfig
 
 echo "=============================================="
