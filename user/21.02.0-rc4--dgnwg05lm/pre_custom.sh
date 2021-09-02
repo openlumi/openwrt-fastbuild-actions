c67d3f6c53d324639c34da65b963c4a5fc1087ce
mrG1K <mr@g1k.ru>
2021-09-02 13:21:40 +0300

    21.02.0 release

--- user/21.02.0-rc4--dgnwg05lm/pre_custom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv .config .config.diff
-cp config_xiaomi_dgnwg05lm .config
-cat .config.diff >> .config
--- user/21.02.0-rc4--zhwg11lm/pre_custom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv .config .config.diff
-cp config_aqara_zhwg11lm .config
-cat .config.diff >> .config
