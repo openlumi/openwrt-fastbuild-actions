7bd0a7ba3034ab363af1c1c68d8435cb20e80daf
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-11-02 11:31:19 +0300

    Build version 21.02.1

---
+++ user/21.02.1--zhwg11lm/custom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mv .config .config.diff
+cp config_aqara_zhwg11lm .config
+cat .config.diff >> .config
+
+
+echo "=============================================="
+rm ./package/feeds/packages/node
+rm ./package/feeds/packages/node-*
+./scripts/feeds install -a -p node
+./scripts/feeds install -a -p node
+make defconfig
+echo "=============================================="
