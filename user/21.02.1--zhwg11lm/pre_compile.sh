64bd299b2b3b5d3b8dd92caba90fe9ace8112714
mrG1K <mr@g1k.ru>
2021-11-05 16:26:44 +0300

    Test build multi_config

--- user/21.02.1--zhwg11lm/pre_compile.sh
+++ user/21.02.1--zhwg11lm/pre_compile.sh
@@ -4,9 +4,7 @@ cp config_xiaomi_dgnwg05lm .config
 cat ${BUILDER_PROFILE_DIR}/.config.diff >> .config
 
 echo "Compiling xiaomi_dgnwg05lm..."
-local nthread=$(($(nproc) + 1))
-echo "${nthread} thread compile: $*"
-make -j${nthread} "$@" V=sc
+make -j$(($(nproc) + 1)) V=sc
 
 echo "Config aqara_zhwg11lm..."
 rm .config
