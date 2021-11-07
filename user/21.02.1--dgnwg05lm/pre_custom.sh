75dc1b06eebee5beb950f546edf9799a3c75e4ee
mrG1K <mr@g1k.ru>
2021-11-07 23:47:41 +0300

    21.02.1

--- user/21.02.1--dgnwg05lm/pre_custom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp feeds.conf.default feeds.conf
-echo "src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-21.02" >> feeds.conf
-echo "src-git openlumi https://github.com/openlumi/openwrt-lumi-packages.git" >> feeds.conf
