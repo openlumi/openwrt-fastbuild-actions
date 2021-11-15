7399d487dc3e877173a753b7693f95f28bfc8876
mrG1K <mr@g1k.ru>
2021-11-15 18:27:47 +0300

    HA build

--- user/21.02.1/custom.sh
+++ user/21.02.1/custom.sh
@@ -5,5 +5,7 @@ rm ./package/feeds/packages/node
 rm ./package/feeds/packages/node-*
 ./scripts/feeds install -a -p node
 ./scripts/feeds install -a -p node
+./scripts/feeds update homeassistant
+./scripts/feeds install -a -p homeassistant
 make defconfig
 echo "=============================================="
