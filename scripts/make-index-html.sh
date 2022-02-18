bed53bc4c758ec723118029a926eaba11d672822
mrG1K <mr@g1k.ru>
2022-02-18 18:40:32 +0300

    fix git file_modify_time

--- scripts/make-index-html.sh
+++ scripts/make-index-html.sh
@@ -105,7 +105,7 @@ function generate_html_index() {
 
       file=`basename "$i"`
       file_size=`du -h "$i" | cut -f1`
-      file_modify_time=`git log -1 --pretty="format:%ci" $i`
+      file_modify_time=`git log -1 --format=%ci -- $i`
 
       CONTENT+='<tr>'
       CONTENT+='<td class="n"><a href="./'$file'">'$file'</a></td>'
