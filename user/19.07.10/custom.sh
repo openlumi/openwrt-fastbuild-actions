9b0bbce665670dc15bc6b29ad2ed805826b2fa05
Ivan Belokobylskiy <belokobylskij@gmail.com>
2022-08-03 21:50:22 +0400

    Build 19.07.10

---
+++ user/19.07.10/custom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mv .config .config.diff
+cp config_xiaomi_lumi .config
+cat .config.diff >> .config
+
+mkdir -p files
+cp -r files_xiaomi_lumi/* files/
+
+echo "=============================================="
+rm ./package/feeds/packages/node
+rm ./package/feeds/packages/node-*
+./scripts/feeds update node
+./scripts/feeds install -a -p node
+make defconfig
+
+echo "=============================================="
---
+++ user/19.07.10/pre_custom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp feeds.conf.default feeds.conf
+echo "src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-19.07" >> feeds.conf
+echo "src-git openlumi https://github.com/openlumi/openwrt-lumi-packages.git" >> feeds.conf
+
