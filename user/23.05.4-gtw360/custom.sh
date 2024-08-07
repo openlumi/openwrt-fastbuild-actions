fef0ebf40870ebbbc5ae959102633621855a8367
Ivan Belokobylskiy <belokobylskij@gmail.com>
2024-08-07 22:43:39 +0300

    23.05: merge gtw360 image

--- user/23.05.4-gtw360/custom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "=============================================="
-rm ./package/feeds/packages/node
-rm ./package/feeds/packages/node-*
-./scripts/feeds update node
-./scripts/feeds install -a -p node
-make defconfig
-echo "=============================================="
--- user/23.05.4-gtw360/pre_custom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp feeds.conf.default feeds.conf
-echo "src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-23.05" >> feeds.conf
-echo "src-git openlumi https://github.com/openlumi/openwrt-lumi-packages.git;23.05" >> feeds.conf
