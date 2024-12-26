2e0389bbd536a3550a46f57d7601cc119c675b2f
Ivan Belokobylskiy <belokobylskij@gmail.com>
2024-12-26 18:55:54 +0400

    23.05: add modem feed

--- user/23.05.5/pre_custom.sh
+++ user/23.05.5/pre_custom.sh
@@ -3,3 +3,4 @@
 cp feeds.conf.default feeds.conf
 echo "src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-23.05" >> feeds.conf
 echo "src-git openlumi https://github.com/openlumi/openwrt-lumi-packages.git;23.05" >> feeds.conf
+echo "src-git modemfeed https://github.com/koshev-msk/modemfeed.git" >> feeds.conf
