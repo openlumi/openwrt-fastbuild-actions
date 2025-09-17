45fd8dbe0cdce5cd9e27223bf0c5ed11480e6c56
Ivan Belokobylskiy <belokobylskij@gmail.com>
2025-09-17 17:30:19 +0400

    fix running node on external ip and port

--- user/24.10.2/custom.sh
+++ user/24.10.2/custom.sh
@@ -5,6 +5,6 @@ rm ./package/feeds/packages/node
 rm ./package/feeds/packages/node-*
 ./scripts/feeds update node
 ./scripts/feeds install -a -p node
-sed -i -E 's/default NODEJS_[[:digit:]]{2}/default NODEJS_20/g' package/feeds/node/node/Makefile
+#sed -i -E 's/default NODEJS_[[:digit:]]{2}/default NODEJS_20/g' package/feeds/node/node/Makefile
 make defconfig
 echo "=============================================="
