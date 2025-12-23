a2da6a24440425a0c30a69ca5d4b9b72bc79a187
Ivan Belokobylskiy <belokobylskij@gmail.com>
2025-12-23 12:47:26 +0400

    25.12: patch versions

--- user/25.12.0/custom.sh
+++ user/25.12.0/custom.sh
@@ -5,5 +5,9 @@ rm ./package/feeds/packages/node
 rm ./package/feeds/packages/node-*
 ./scripts/feeds update node
 ./scripts/feeds install -a -p node
+
+sed -i -e 's/PKG_VERSION:=.*/PKG_VERSION:=0.4.0/' -e 's/PKG_HASH:=.*/PKG_HASH:=skip/' feeds/packages/lang/python/python-aio-mqtt-mod/Makefile
+sed -i -e 's/PKG_VERSION:=.*/PKG_VERSION:=0.2.5/' -e 's/PKG_HASH:=.*/PKG_HASH:=skip/' feeds/packages/lang/python/python-ble2mqtt/Makefile
+
 make defconfig
 echo "=============================================="
