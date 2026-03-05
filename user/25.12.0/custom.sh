1d03b13c068693ea8969d55fea582817139a3464
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-03-05 16:23:57 +0400

    25.12: bump python-evdev

--- user/25.12.0/custom.sh
+++ user/25.12.0/custom.sh
@@ -7,6 +7,7 @@ rm ./package/feeds/packages/node-*
 ./scripts/feeds install -a -p node
 
 sed -i -e 's/PKG_VERSION:=.*/PKG_VERSION:=0.2.5/' -e 's/PKG_HASH:=.*/PKG_HASH:=skip/' feeds/packages/lang/python/python-ble2mqtt/Makefile
+sed -i -e 's/PKG_VERSION:=.*/PKG_VERSION:=1.9.3/' -e 's/PKG_HASH:=.*/PKG_HASH:=skip/' feeds/packages/lang/python/python-evdev/Makefile
 
 make defconfig
 echo "=============================================="
