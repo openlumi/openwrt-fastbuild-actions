e4aeee10e55070bcfbc644c09683cd6530835cf4
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-03-25 20:52:49 +0400

    24.10.6: initial build

---
+++ user/24.10.6/pre_custom.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp feeds.conf.default feeds.conf
+echo "src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-24.10" >> feeds.conf
+echo "src-git openlumi https://github.com/openlumi/openwrt-lumi-packages.git;23.05" >> feeds.conf
